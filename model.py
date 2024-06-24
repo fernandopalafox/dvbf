00743cb6dcd88f0297dae8cc152731cb3c71aa1a
fepahri <fernando_palafoxescobedo@honda-ri.com>
2024-06-24 11:47:32 -0400

    added initial network and observation net

---
+++ model.py
@@ -0,0 +1,35 @@
+import jax
+import jax.numpy as jnp
+import flax
+from flax import linen as nn
+
+
+class InitialNetwork(nn.Module):
+    latent_dim: int
+
+    @nn.compact
+    def __call__(self, xs):
+        # Simple LSTM network
+        lstm = nn.OptimizedLSTMCell(128)
+        x_seq = jnp.transpose(x_seq, (1, 0, 2))  # (T, B, D)
+        _, lstm_state = nn.RNN(lstm)(x_seq)
+        output = lstm_state.hidden
+
+        # Output mean and logvar for w_1
+        x = nn.Dense(128)(output)
+        x = nn.relu(x)
+        params = nn.Dense(2 * self.latent_dim)(x)
+        mean, logvar = jnp.split(params, 2, axis=-1)
+        return mean, logvar
+
+
+class Observation(nn.Module):
+    obs_dim: int
+
+    @nn.compact
+    def __call__(self, z):
+        # 128 ReLU + 16^2 identity output
+        x = nn.Dense(128)(z)
+        x = nn.relu(x)
+        mean = nn.Dense(self.obs_dim)(x)
+        return mean
