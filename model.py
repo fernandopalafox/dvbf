4cc0b86bfa2f5d0e6bb85ed0b607439e3a024d67
fepahri <fernando_palafoxescobedo@honda-ri.com>
2024-06-24 15:55:02 -0400

    first try at DVBF

--- model.py
+++ model.py
@@ -76,3 +76,84 @@ class TransitionWeights(nn.Module):
         )(input)
         alphas = nn.softmax(alphas)
         return alphas
+
+
+class DVBF(nn.Module):
+    latent_dim: int
+    obs_dim: int
+    control_dim: int
+    num_matrices: int
+
+    @nn.compact
+    def __call__(self, xs, us):
+        # Initialize model components
+        initial_network = InitialNetwork(self.latent_dim)
+        initial_transition = InitialTransition(self.latent_dim)
+        observation = Observation(self.obs_dim)
+        recognition = Recognition(self.latent_dim)
+        transition_weights = TransitionWeights(
+            self.latent_dim, self.control_dim
+        )
+
+        # Sample initial network
+        w_1_mean, w_1_logvar = initial_network(xs)
+        key = self.make_rng("rng_stream")
+        w_1 = w_1_mean + jnp.exp(w_1_logvar / 2) * jax.random.normal(
+            key, w_1_mean.shape
+        )
+
+        # Compute initial transition
+        z_1 = initial_transition(w_1)
+
+        # Transition matrices
+        As = self.param(
+            "A",
+            nn.initializers.normal(0.01),
+            (self.num_matrices, self.latent_dim, self.latent_dim),
+        )
+        Bs = self.param(
+            "B",
+            nn.initializers.normal(0.01),
+            (self.num_matrices, self.latent_dim, self.control_dim),
+        )
+        Cs = self.param(
+            "C",
+            nn.initializers.normal(0.01),
+            (self.num_matrices, self.obs_dim, self.latent_dim),
+        )
+
+        # Compute sequence of states
+        zs = [z_1]
+        xs_reconstructed = []
+        for t in range(xs.shape[0] - 1):
+            # Compute observation
+            x_mean = observation(zs[-1])
+            xs_reconstructed.append(x_mean)
+
+            # Compute next latent state
+            # Sample stochastic component
+            w_mean, w_logvar = recognition(zs[-1], xs[t + 1], us[t])
+            key = self.make_rng("rng_stream")
+            w = w_mean + jnp.exp(w_logvar / 2) * jax.random.normal(
+                key, w_mean.shape
+            )
+            # Compute deterministic component
+            alphas = transition_weights(zs[-1], us[t])
+            A_t = jnp.einsum("bi,ijk-> bjk", alphas, As)
+            B_t = jnp.einsum("bi,ijk-> bjk", alphas, Bs)
+            C_t = jnp.einsum("bi,ijk-> bjk", alphas, Cs)
+            # Compute next latent state
+            z_t_plus_one = (
+                jnp.einsum("bjk, bk -> bj", A_t, zs[-1])
+                + jnp.einsum("bjk, bk -> bj", B_t, us[t])
+                + jnp.einsum("bjk, bk -> bj", C_t, w)
+            )
+            zs.append(z_t_plus_one)
+
+            # Compute next observation
+            x_mean = observation(z_t_plus_one)
+
+        zs = jnp.vstack(zs)
+        xs_reconstructed = jnp.vstack(xs_reconstructed)
+
+        return zs, xs_reconstructed
