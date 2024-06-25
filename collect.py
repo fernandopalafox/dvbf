ed0247fccaa0e26817005ea9fb0d91c0bbb7829c
fepahri <fernando_palafoxescobedo@honda-ri.com>
2024-06-25 12:44:22 -0400

    moved things around

--- collect.py
+++ collect.py
@@ -1,44 +1,14 @@
 import gymnasium as gym
-from gymnasium.wrappers import PixelObservationWrapper
-from gym import spaces
-import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+from gymnasium.wrappers import PixelObservationWrapper
+from wrapper import ResizePixelObservation
 
+# Data collection parameters
+num_sequences = 100
+sequence_length = 15
+image_size = 16
 
-class ResizePixelObservation(gym.ObservationWrapper):
-    def __init__(self, env, shape):
-        gym.utils.RecordConstructorArgs.__init__(self, shape=shape)
-        gym.ObservationWrapper.__init__(self, env)
-
-        if isinstance(shape, int):
-            shape = (shape, shape)
-        self.shape = tuple(shape)
-        spaces_dict = self.observation_space.spaces
-        old_pixel_space = spaces_dict["pixels"]
-        spaces_dict["pixels"] = spaces.Box(
-            low=0,
-            high=255,
-            shape=self.shape + (old_pixel_space.shape[2],),
-            dtype=np.uint8,
-        )
-        self.observation_space = spaces.Dict(spaces_dict)
-
-    def observation(self, observation):
-        obs_dict = dict(observation)
-        pixels = obs_dict["pixels"]
-        resized_pixels = cv2.resize(
-            pixels,
-            self.shape[::-1],
-            interpolation=cv2.INTER_AREA,
-        )
-        if resized_pixels.ndim == 2:
-            resized_pixels = np.expand_dims(resized_pixels, -1)
-        obs_dict["pixels"] = resized_pixels
-        return obs_dict
-
-
-# Test the pixel observation wrapper
+# Define environment
 env = ResizePixelObservation(
     PixelObservationWrapper(
         gym.make("Pendulum-v1", g=9.81, render_mode="rgb_array")
@@ -46,9 +16,14 @@ env = ResizePixelObservation(
     16,
 )
 observation, info = env.reset()
-print(observation["pixels"].shape, info)
-env.close()
 
-plt.figure()
-plt.imshow(observation["pixels"])
-plt.show()
+# observations = [observation]
+# for j in range(sequence_length - 1):
+#     action = env.action_space.sample()
+#     observation, reward, terminated, truncated, info = env.step(action)
+
+#     if terminated or truncated:
+#         observation, info = env.reset()
+
+
+env.close()
---
+++ wrapper.py
@@ -0,0 +1,38 @@
+import gymnasium as gym
+from gymnasium.wrappers import PixelObservationWrapper
+from gym import spaces
+import numpy as np
+import cv2
+
+
+class ResizePixelObservation(gym.ObservationWrapper):
+    def __init__(self, env, shape):
+        gym.utils.RecordConstructorArgs.__init__(self, shape=shape)
+        gym.ObservationWrapper.__init__(self, env)
+
+        if isinstance(shape, int):
+            shape = (shape, shape)
+        self.shape = tuple(shape)
+        spaces_dict = self.observation_space.spaces
+        old_pixel_space = spaces_dict["pixels"]
+        spaces_dict["pixels"] = spaces.Box(
+            low=0,
+            high=255,
+            shape=self.shape + (old_pixel_space.shape[2],),
+            dtype=np.uint8,
+        )
+        self.observation_space = spaces.Dict(spaces_dict)
+
+    def observation(self, observation):
+        obs_dict = dict(observation)
+        pixels = obs_dict["pixels"]
+        resized_pixels = cv2.resize(
+            pixels,
+            self.shape[::-1],
+            interpolation=cv2.INTER_AREA,
+        )
+        grayscale_pixels = cv2.cvtColor(resized_pixels, cv2.COLOR_RGB2GRAY)
+        if grayscale_pixels.ndim == 2:
+            grayscale_pixels = np.expand_dims(grayscale_pixels, -1)
+        obs_dict["pixels"] = grayscale_pixels
+        return obs_dict
